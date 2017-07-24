b5e48a47d0cbfa15621c94de47e97995136b4db1
sunlibocs <sunlibocs@gmail.com>
2017-07-24 14:33:20 +0800

    tets1

--- lsd_slam_core/src/SlamSystem.cpp
+++ lsd_slam_core/src/SlamSystem.cpp
@@ -17,7 +17,7 @@
 * You should have received a copy of the GNU General Public License
 * along with LSD-SLAM. If not, see <http://www.gnu.org/licenses/>.
 */
-//test here
+//test here  11111111
 #include "SlamSystem.h"
 
 #include "DataStructures/Frame.h"
